ce2b20f09a64f3bbf99304d302b28403732b3967
Roman Shelestov <m9icjkeee@gmail.com>
2023-07-17 14:53:16 +0400

    changes in conftest + .gitkeep for tmp directory

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,16 +1,19 @@
 import csv
 import os.path
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
+csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources/eggs.csv'))
+
 
 def test_csv():
-    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources/eggs.csv'))
-    with open(csv_path, 'w') as csvfile:
+    with open(csv_path, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
         csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-    with open(csv_path) as csvfile:
-        csvreader = csv.reader(csvfile)
+
+def test_csv_2():
+    with open(csv_path) as opencsv:
+        csvreader = csv.reader(opencsv)
     for row in csvreader:
         assert len(row) == 3
     os.remove(csv_path)
